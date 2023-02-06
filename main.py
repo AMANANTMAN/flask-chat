1ec84c7767675f4b404c376720a0cee712a5bde4
Anthony Norris <83408471+AMANANTMAN@users.noreply.github.com>
2023-02-06 15:26:33 +0000

    Added updateBoard

--- main.py
+++ main.py
@@ -5,26 +5,29 @@ app = Flask(__name__)
 
 board = chess.Board()
 
-print_board = ''
-for i in range(0, 64, 8):
+def updateBoard()
+  print_board = ''
+  for i in range(0, 64, 8):
     print_board += ' '.join(board.split(' ')[i:i+8]) + '\n'
+  return print_board
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', embed=board)
+  embed = updateBoard()
+  return render_template('index.html', embed=embed)
  
 @app.route('/form')
 def form():
-    return render_template('form.html')
+  return render_template('form.html')
  
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+  if request.method == 'GET':
+    return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+  if request.method == 'POST':
+    form_data = request.form
+    return render_template('data.html',form_data = form_data)
 
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
