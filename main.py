f10702b8181011ba4df2db7e4a603d6191a9c898
Anthony Norris <83408471+AMANANTMAN@users.noreply.github.com>
2023-02-06 16:03:51 +0000

    A lot

--- main.py
+++ main.py
@@ -6,10 +6,28 @@ app = Flask(__name__)
 board = chess.Board()
 
 def updateBoard():
-  temp = board.split(" ")
-  print_board = ''
-  for i in range(0, 64, 8):
-    print_board += ' '.join(temp[i:i+8]) + '\n'
+  def make_matrix(board): #type(board) == chess.Board()
+    pgn = board.epd()
+    foo = []  #Final board
+    pieces = pgn.split(" ", 1)[0]
+    rows = pieces.split("/")
+    for row in rows:
+      foo2 = []  #This is the row I make
+      for thing in row:
+        if thing.isdigit():
+          for i in range(0, int(thing)):
+            foo2.append('.')
+        else:
+          foo2.append(thing)
+      foo.append(foo2)
+    return foo
+  new_board = make_matrix(board)
+  print_board = ""
+  for list in new_board:
+    for i in list:
+      print_board += i+" " 
+    print_board += "\n"
+
   return print_board
 
 @app.route('/')
