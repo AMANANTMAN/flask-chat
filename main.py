ad342122c8d785291f9d91e1ca762cc418130d57
Anthony Norris <83408471+AMANANTMAN@users.noreply.github.com>
2023-02-06 15:28:19 +0000

    :

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 board = chess.Board()
 
-def updateBoard()
+def updateBoard():
   print_board = ''
   for i in range(0, 64, 8):
     print_board += ' '.join(board.split(' ')[i:i+8]) + '\n'
