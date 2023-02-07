7bf450bfc39f2abdac0ea22a61f8a0fd59f499c8
Anthony Norris <83408471+AMANANTMAN@users.noreply.github.com>
2023-02-07 16:04:11 +0000

    moreshit

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ app = Flask(__name__)
 
 board = chess.Board()
 
-def updateBoard():
-  def make_matrix(board): #type(board) == chess.Board()
-    pgn = board.epd()
-    foo = []  #Final board
-    pieces = pgn.split(" ", 1)[0]
-    rows = pieces.split("/")
-    for row in rows:
-      foo2 = []  #This is the row I make
-      for thing in row:
-        if thing.isdigit():
-          for i in range(0, int(thing)):
-            foo2.append('.')
-        else:
-          foo2.append(thing)
-      foo.append(foo2)
-    return foo
-  make_matrix(board)
+def make_matrix(board): #type(board) == chess.Board()
+  pgn = board.epd()
+  foo = []  #Final board
+  pieces = pgn.split(" ", 1)[0]
+  rows = pieces.split("/")
+  for row in rows:
+    foo2 = []  #This is the row I make
+    for thing in row:
+      if thing.isdigit():
+        for i in range(0, int(thing)):
+          foo2.append('.')
+      else:
+        foo2.append(thing)
+    foo.append(foo2)
+  return foo
 
+test = ""
+for board in make_matrix(board):
+  for item in board:
+    test += item + " "
+  test += "\n"
   
 
 @app.route('/')
 def home_page():
-  embed = board
+  embed = test
   return render_template('index.html', content_type="text/plain",embed=embed)
  
 @app.route('/form')
